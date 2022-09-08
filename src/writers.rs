6ece572344848fea9858d4a7118891dd753dcb2d
Andrei Gherzan <andrei@gherzan.com>
2022-09-08 11:42:03 +0100

    Bump dependencies to fix yet another security vulnerability
    Signed-off-by: Andrei Gherzan <andrei@gherzan.com>

--- src/writers.rs
+++ src/writers.rs
@@ -1,15 +1,14 @@
 use image::ColorType;
-use image::png::PNGEncoder;
+use image::codecs::png::PngEncoder;
 use std::fs::File;
-use std::io::Error;
 
 // Writes a png encoded file of a pixel map array
-pub fn export_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), Error> {
+pub fn export_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> image::ImageResult<()> {
     let file = File::create(filename)?;
-    let encoder = PNGEncoder::new(file);
+    let encoder = PngEncoder::new(file);
     encoder.encode(&pixels,
                    bounds.0 as u32,
                    bounds.1 as u32,
-                   ColorType::Gray(8))?;
+                   ColorType::L8)?;
     Ok(())
 }
